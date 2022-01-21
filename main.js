318712b7b8f7f1f07ed11d7982e806e5fe2f1e41
Gunvant-Sai <79575963+Gunvant-Sai@users.noreply.github.com>
2022-01-21 16:06:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var video = "";
+var status1 = "";
+var object_name = "";
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function setup() 
+{
+    canvas = createCanvas(480,480);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function Start(){
+    objectdecter = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("Op").innerHTML = "Status: Detecting Objects";
+    object_name = document.getElementById("Object").value;
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status1=true;
+}
+
+function gotresult(error,result)
+{
+  if(error)
+  {
+      console.log(error);
+  }
+    console.log(result);
+    baby= result;
+}
+
+function draw() {
+    image(video,0,0,580,480);
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    objectDetector.detect(video,gotresult);
+    if(status_ != "")
+    {
+        if(object.length < 0)
+        {
+            document.getElementById("GO").innerHTML = object_name+"Not Found";
+        }
+      for(i= 0; i < object.length; i++)
+      {
+          r = random(255);
+          g = random(255);
+          b = random(255);
+
+          document.getElementById("GO").innerHTML = "Status: Detecting Objects";
+
+          fill(r,g,b);
+          percent = floor(object[i].confidence*100);
+          text(object[i].label, object[i].x +15,object[i].y +15);
+          noFill();
+          stroke(r,g,b);
+          rect(object[i].x , object[i].y , object[i].width , object[i].height);
+          if(objects[i].label == object_name)
+          {
+           document.getElementById("GO").innerHTML = object_name+"Found";
+           video.stop();
+           objectDetector.detect(gotresult);
+           var synth = window.speechSynthesis;
+           speak_data = "Object Found";
+           var utterThis = new SpeechSynthesisUtterance(speak_data);
+           synth.speak(utterThis);
+          }
+          else{
+            document.getElementById("GO").innerHTML = object_name+"Not Found"; 
+          }
+      }
+    }
+}
